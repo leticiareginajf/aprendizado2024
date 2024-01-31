f1f4bfd5d4e1e8d26918ae98b1e35fd4a2b3e56f
leticiareginajf <leticia.reginajf@ggmail.com>
2024-01-31 16:07:20 -0300

    atualizando classe While

---
+++ aprendizados/src/aprendizados/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package aprendizados;
+
+public class WhileDeterminado {
+
+    public static void main(String args[]){
+
+        int contador = 1;
+
+        while(contador <= 20){
+            System.out.println(contador + "-" + "" + "Bom dia");
+            contador+=2;
+        }
+    }
+}
